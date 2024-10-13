e84fb19401cb06ca987eafd5c3cdddd9abc80425
Caio Henrique <104094613+caiohenrique-3@users.noreply.github.com>
2024-10-13 16:15:31 -0300

    feat: basic security implementation

--- src/main/java/com/example/controllers/MainController.java
+++ src/main/java/com/example/controllers/MainController.java
@@ -10,10 +10,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Optional;
@@ -98,5 +95,11 @@ public class MainController {
                 threadService.getTimeSinceLastThread());
 
         return "stats";
+    }
+
+    @GetMapping("/admin")
+    @ResponseBody
+    public String showAdminPage(Model model) {
+        return "Hello Admin!";
     }
 }
