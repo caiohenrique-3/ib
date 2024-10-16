1f2e5ba78cad7426fd21ae7ea4b98b4196527a62
Caio Henrique <104094613+caiohenrique-3@users.noreply.github.com>
2024-10-16 17:20:31 -0300

    fix: Add csrf to broken tests

--- src/test/java/com/example/controllers/MainControllerTests.java
+++ src/test/java/com/example/controllers/MainControllerTests.java
@@ -14,10 +14,14 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
+import org.springframework.security.web.csrf.DefaultCsrfToken;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.request.RequestPostProcessor;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -52,7 +56,7 @@ public class MainControllerTests {
 
     @Test
     void testShowMainPage_notAllowed_ifPost() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.post("/"))
+        mockMvc.perform(MockMvcRequestBuilders.post("/").with(csrf()))
                 .andExpect(MockMvcResultMatchers.view().name("error"));
     }
 
@@ -60,7 +64,8 @@ public class MainControllerTests {
     void testCreateThread() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.post("/createThread")
                         .param("title", "Test Title")
-                        .param("body", "Test Body"))
+                        .param("body", "Test Body")
+                        .with(csrf()))
                 .andExpect(MockMvcResultMatchers.status().is3xxRedirection())
                 .andExpect(MockMvcResultMatchers.redirectedUrl("/"));
     }
@@ -94,7 +99,8 @@ public class MainControllerTests {
 
         mockMvc.perform(MockMvcRequestBuilders.post("/replyTo/" + t.getId())
                         .param("id", String.valueOf(t.getId()))
-                        .param("body", "Test Body"))
+                        .param("body", "Test Body")
+                        .with(csrf()))
                 .andExpect(MockMvcResultMatchers.status().is3xxRedirection())
                 .andExpect(MockMvcResultMatchers.redirectedUrl("/threads/" + t.getId()));
     }
@@ -103,7 +109,8 @@ public class MainControllerTests {
     void testCreateReply_throwsError_ifNotFound() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.post("/replyTo/-1")
                         .param("id", String.valueOf(-1))
-                        .param("body", "Test Body"))
+                        .param("body", "Test Body")
+                        .with(csrf()))
                 .andExpect(MockMvcResultMatchers.view().name("error"));
     }
 
@@ -114,7 +121,8 @@ public class MainControllerTests {
         Post p2 = postService.createPostAndReturn("test", t.getId(), null);
         Post p3 = postService.createPostAndReturn("test", t.getId(), null);
 
-        mockMvc.perform(MockMvcRequestBuilders.get("/stats"))
+        mockMvc.perform(MockMvcRequestBuilders.get("/stats")
+                        .with(csrf()))
                 .andExpect(MockMvcResultMatchers.view().name("stats"))
                 .andExpect(MockMvcResultMatchers.model().attribute("totalThreads", 1L))
                 .andExpect(MockMvcResultMatchers.model().attribute("totalPosts", 3L))
